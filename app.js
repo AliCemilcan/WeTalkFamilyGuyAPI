d464840699a6edf98da7335f9a89405c04ef0ffc
alicemilcan <ciftarsl@sheridancollege.ca>
2022-01-09 21:19:31 -0500

    port error fix

--- app.js
+++ app.js
@@ -60,4 +60,6 @@ const connect = () => {
 }
 connect()
 //start listening the server
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
